378ff0c47e40671fecf92ac62998cf3eabc6bc83
Alex Maldonado <alex@Alexs-MBP-3.home>
2021-11-23 05:34:19 -0800

    connect to  deployed strapi api

--- pages/[slug].js
+++ pages/[slug].js
@@ -2,52 +2,54 @@ import Link from 'next/link';
 import React from 'react';
 import MarkdownIt from 'markdown-it';
 
-export default function Post() {
-  return <h1>Post</h1>;
+export default function Post({ post }) {
+  const md = new MarkdownIt();
+  const htmlContent = md.render(post.Content);
+  console.log(htmlContent);
+  return (
+    <article>
+      <div className='container mx-auto p-10 flex flex-col items-center'>
+        {/* <h1 className='text-3xl'>{post.Title}</h1>
+        <h3>{post.User.username}</h3> */}
+        <section
+          className='prose prose-dark'
+          dangerouslySetInnerHTML={{ __html: htmlContent }}
+        ></section>
+        <h1>Article content coming soon...</h1>
+        <Link href='/'>
+          <a className='text-blue-500 underline hover:text-blue-700 text-center'>
+            Back Home
+          </a>
+        </Link>
+      </div>
+    </article>
+  );
 }
 
-// export default function Post({ post }) {
-//   const md = new MarkdownIt();
-//   const htmlContent = md.render(post.Content);
-//   console.log(htmlContent);
-//   return (
-//     <article>
-//       <div className='container mx-auto p-10 flex flex-col items-center'>
-//         <h1 className='text-3xl'>{post.Title}</h1>
-//         <h3>{post.User.username}</h3>
-//         <section
-//           className='prose prose-dark'
-//           dangerouslySetInnerHTML={{ __html: htmlContent }}
-//         ></section>
-//       </div>
-//     </article>
-//   );
-// }
+//tell next.js how many pages there are
+export async function getStaticPaths() {
+  const res = await fetch('https://alexmdev.herokuapp.com/posts');
+  const posts = await res.json();
 
-// //tell next.js how many pages there are
-// export async function getStaticPaths() {
-//   const res = await fetch('http://localhost:1337/posts');
-//   const posts = await res.json();
+  //get slugs from each post
+  const paths = posts.map((post) => ({ params: { slug: post.Slug } }));
 
-//   //get slugs from each post
-//   const paths = posts.map((post) => ({ params: { slug: post.Slug } }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// }
+//for each individual page: get the data for taht page
+export async function getStaticProps({ params }) {
+  const { slug } = params;
 
-// //for each individual page: get the data for taht page
-// export async function getStaticProps({ params }) {
-//   const { slug } = params;
+  const res = await fetch(`https://alexmdev.herokuapp.com/posts?Slug=${slug}`);
+  const data = await res.json();
 
-//   const res = await fetch(`http://localhost:1337/posts?Slug=${slug}`);
-//   const data = await res.json();
+  const post = data[0];
 
-//   const post = data[0];
-
-//   return {
-//     props: { post },
-//   };
-// }
+  return {
+    props: { post },
+  };
+}
--- pages/docs.js
+++ pages/docs.js
@@ -2,9 +2,9 @@ import Link from 'next/link';
 
 export default function Docs({ posts }) {
   return (
-    <div className='container px-6 py-4 mx-auto'>
-      <h1 className='text-6xl font-normal leading-normal mt- mb-2 text-center'>
-        Project Documentation Coming soon...
+    <div className='container py-4 mx-auto max-w-3xl'>
+      <h1 className='text-5xl font-normal leading-normal  mb-2 text-center'>
+        Project Documentation
       </h1>
       <div className='text-center'>
         <Link href='/'>
@@ -13,39 +13,52 @@ export default function Docs({ posts }) {
           </a>
         </Link>
       </div>
-      {/* {posts &&
+      {posts &&
         posts.map((post, idx) => (
           <div
             key={post.id}
-            className='flex flex-col justify-items-start items-centerbg-gray-500 m-10'
+            className='flex flex-col justify-items-start items-centerbg-gray-500 m-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105'
           >
-            <small>{new Date(post.User.created_at).toDateString()}</small>
-            <h1 className='text-3xl text-gray-800 underline font-normal'>
+            <div className='flex flex-col '>
+              <div>
+                <img
+                  className='object-scale-down round-md'
+                  src={post.blogImg.formats.thumbnail.url}
+                  alt=''
+                />
+              </div>
+              <div className=' mb-2 flex flex-col'>
+                <small className='dark:text-gray-300'>
+                  {new Date(post.User.createdAt).toDateString()}
+                </small>
+                <small className='mr-5 dark:text-gray-300'>
+                  {post.User.username}
+                </small>
+              </div>
+            </div>
+            <h1 className='text-4xl text-gray-800  font-normal'>
               {post.Title}
             </h1>
-            <div className='flex mb-2'>
-              <small className='mr-5'>{post.User.username}</small>
-            </div>
-            <h3>{post.description}</h3>
+            <h3 className='text-gray-600'>{post.description}</h3>
 
             <Link href={`/${post.Slug}`}>
               <a className='text-blue-500 underline hover:text-blue-700'>
                 Read more...
               </a>
             </Link>
-            <hr class='mt-5 border-0 bg-blue-500 text-blue-500 h-px' />
+            <hr className='mt-5 border-0 bg-blue-500 text-blue-500 h-px' />
           </div>
-        ))} */}
+        ))}
     </div>
   );
 }
 
-// export async function getStaticProps() {
-//   //get posts from API
-//   const res = await fetch('http://localhost:1337/posts');
-//   const posts = await res.json();
+export async function getStaticProps() {
+  //get posts from API
+  const res = await fetch('https://alexmdev.herokuapp.com/posts');
+  const posts = await res.json();
 
-//   return {
-//     props: { posts },
-//   };
-// }
+  return {
+    props: { posts },
+  };
+}
