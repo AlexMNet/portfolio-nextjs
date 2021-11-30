59bafcc95de3ccb676a0cb11e5701af65e5803bb
Alex Maldonado <alex@Alexs-MBP-3.home>
2021-11-30 14:09:07 -0800

    update blog styles

--- pages/[slug].js
+++ pages/[slug].js
@@ -5,11 +5,27 @@ import MarkdownIt from 'markdown-it';
 export default function Post({ post }) {
   const md = new MarkdownIt();
   const htmlContent = md.render(post.Content);
+
   return (
     <article>
-      <div className='container mx-auto p-10 items-center w-screen'>
+      <div className='container mx-auto p-10 justify-center items-start text-left flex flex-col '>
+        <div className='mb-5'>
+          <h1 className='text-4xl md:text-6xl'>{post.Title}</h1>
+          <small>
+            by: <span className='text-blue-500'>{post.User.username}</span>
+          </small>
+          <small className='block'>
+            {new Date(post.createdAt).toDateString()}
+          </small>
+          <div className='flex gap-2 w-20'>
+            {post.badges.map(({ id, url }) => (
+              <img key={id} src={url} alt='' />
+            ))}
+          </div>
+        </div>
+        <img src={post.blogImg.url} alt='' style={{ width: '400px' }} />
         <article
-          className='prose dark:prose-dark prose-blue lg:prose-xl '
+          className='prose dark:prose-dark prose-blue'
           dangerouslySetInnerHTML={{ __html: htmlContent }}
         ></article>
         <Link href='/'>
