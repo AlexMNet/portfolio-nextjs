146839c3e3d0fd95179c325d15f7c5586a1350fc
Alex Maldonado <alex@Alexs-MBP-3.home>
2021-11-29 21:26:15 -0800

    clean up old code

--- pages/_app.js
+++ pages/_app.js
@@ -19,11 +19,9 @@ function MyApp({ Component, pageProps }) {
         variants={{
           initial: {
             opacity: 0,
-            // x: '-100%',
           },
           animate: {
             opacity: 1,
-            // x: '0%',
           },
         }}
         exit={{ opacity: 0 }}
--- pages/about.js
+++ pages/about.js
@@ -2,9 +2,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { RoughNotation } from 'react-rough-notation';
 import { useInView } from 'react-intersection-observer';
-import withTransition from '../HOC/withTransition';
 
-function About() {
+export default function About() {
   //useInView is used to detect if a div or ele is in view.
   //inView will  be true when in view and can be used to se animations
   const { ref, inView, entry } = useInView({
@@ -103,5 +102,3 @@ function About() {
     </section>
   );
 }
-
-export default withTransition(About);
