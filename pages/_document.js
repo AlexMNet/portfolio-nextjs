f88f3a995753d99531604a7698d4fdb54f640305
Alex Maldonado <alex@Alexs-MBP-3.home>
2021-12-09 17:58:05 -0800

    add website preview image to html head

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,10 @@ class MyDocument extends Document {
     return (
       <Html className='h-full'>
         <Head />
+        <meta
+          property='og:image'
+          content='https://res.cloudinary.com/dtbtmmgxx/image/upload/v1639101147/large_dev_portfolio_dark_43823a7947.png'
+        />
         <body className='bg-gray-100 dark:bg-black relative min-h-screen flex flex-col'>
           <Main className='flex-auto' />
           <NextScript />
