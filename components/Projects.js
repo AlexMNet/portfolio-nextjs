20aa1aeca91f80671e42d6af9f95a5c6b739e6c0
Alex Maldonado <alex@Alexs-MBP-3.home>
2021-12-14 00:16:38 -0800

    Refactor projects and technolgoies component

--- components/Projects.js
+++ components/Projects.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
+import React from 'react';
 import ScrollAnimation from 'react-animate-on-scroll';
+import { showcasedProjects } from '../utils/data/showcasedProjects';
 
 export default function Projects() {
   return (
@@ -13,132 +15,53 @@ export default function Projects() {
         </h1>
         <hr className='border-0 bg-blue-500 text-blue-500 h-px max-w-md mx-auto' />
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-x-10 xl-grid-cols-4 gap-y-10 gap-x-6 mt-5 '>
-          {/* Card */}
-          <ScrollAnimation
-            animateIn='fadeInUp'
-            animateOut='fadeOutUp'
-            animateOnce={true}
-          >
-            <div className='overflow-hidden container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300 bg-white'>
-              <Link href='/dev-portfolio-site'>
-                <img
-                  src='/images/dev-portfolio.png'
-                  alt=''
-                  className=' w-full'
-                />
-              </Link>
-              <div className='p-6 border-t-2 border-blue-500 dark:bg-black overflow-hidden'>
-                <h1 className='md:text-1xl text-xl hover:text-indigo-600 transition duration-200  font-bold text-gray-900 dark:text-gray-400'>
-                  Dev Portfolio
-                </h1>
-                <p className='text-gray-700 my-2 hover-text-900 '>
-                  Built this Dev Portfolio to showcase my work.
-                </p>
-                <span className='text-xs font-semibold inline-block py-1 px-2 rounded text-white bg-blue-500 dark:bg-gray-700 uppercase last:mr-0 mr-1'>
-                  NextJS
-                </span>
-                <span className='text-xs font-semibold inline-block py-1 px-2 rounded text-white bg-blue-500 dark:bg-gray-700 uppercase last:mr-0 mr-1'>
-                  TailwindCSS
-                </span>
-                {/* Github and project links */}
-                <div className='mt-3 flex justify-start'>
-                  <Link href='https://www.alexmaldonado.dev'>
-                    <a className='text-blue-500 underline mr-5' target='_blank'>
-                      Live
-                    </a>
+          {/* Cards */}
+          {showcasedProjects.map(
+            ({ id, img, docLink, title, desc, tech, liveLink, githubLink }) => (
+              <ScrollAnimation
+                key={id}
+                animateIn='fadeInUp'
+                animateOut='fadeOutUp'
+                animateOnce={true}
+              >
+                <div className='overflow-hidden container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300 bg-white'>
+                  <Link href={docLink}>
+                    <img src={img} alt='' className=' w-full' />
                   </Link>
-                  <Link href='https://github.com/AlexMNet/portfolio-nextjs'>
-                    <a className='text-blue-500 underline' target='_blank'>
-                      Github
-                    </a>
-                  </Link>
+                  <div className='p-6 border-t-2 border-blue-500 dark:bg-black overflow-hidden'>
+                    <h1 className='md:text-1xl text-xl hover:text-indigo-600 transition duration-200  font-bold text-gray-900 dark:text-gray-400'>
+                      {title}
+                    </h1>
+                    <p className='text-gray-700 my-2 hover-text-900 '>{desc}</p>
+                    {tech.map((title, idx) => (
+                      <span
+                        key={idx}
+                        className='text-xs font-semibold inline-block py-1 px-2 rounded text-white bg-blue-500 dark:bg-gray-700 uppercase last:mr-0 mr-1'
+                      >
+                        {title}
+                      </span>
+                    ))}
+                    {/* Github and project links */}
+                    <div className='mt-3 flex justify-start'>
+                      <Link href={liveLink}>
+                        <a
+                          className='text-blue-500 underline mr-5'
+                          target='_blank'
+                        >
+                          Live
+                        </a>
+                      </Link>
+                      <Link href={githubLink}>
+                        <a className='text-blue-500 underline' target='_blank'>
+                          Github
+                        </a>
+                      </Link>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </ScrollAnimation>
-          {/* Card */}
-          <ScrollAnimation
-            animateIn='fadeInUp'
-            animateOut='fadeOutUp'
-            animateOnce={true}
-          >
-            <div className='overflow-hidden container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300 bg-white'>
-              <Link href='/'>
-                <img
-                  src='/images/plate-calculator.jpg'
-                  alt=''
-                  className=' w-full'
-                />
-              </Link>
-              <div className='p-6 border-t-2 border-blue-500 dark:bg-black overflow-hidden'>
-                <h1 className='md:text-1xl text-xl hover:text-indigo-600 transition duration-200  font-bold text-gray-900 dark:text-gray-400'>
-                  Plate Calculator
-                </h1>
-                <p className='text-gray-700 my-2 hover-text-900 '>
-                  A mobile application that calculates plates to put on a bar
-                </p>
-                <span className='text-xs font-semibold inline-block py-1 px-2 rounded text-white bg-blue-500 dark:bg-gray-700 uppercase last:mr-0 mr-1'>
-                  React-Native
-                </span>
-                {/* Github and project links */}
-                <div className='mt-3 flex justify-start'>
-                  <Link href='https://expo.dev/@alexmnet/platecalculator'>
-                    <a className='text-blue-500 underline mr-5' target='_blank'>
-                      Live on Expo Go
-                    </a>
-                  </Link>
-                  <Link href='https://github.com/AlexMNet/plateCalculator-React_Native'>
-                    <a className='text-blue-500 underline' target='_blank'>
-                      Github
-                    </a>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </ScrollAnimation>
-          {/* Card */}
-          <ScrollAnimation
-            animateIn='fadeInUp'
-            animateOut='fadeOutUp'
-            animateOnce={true}
-          >
-            <div className='overflow-hidden container mx-auto shadow-lg rounded-lg max-w-md hover:shadow-2xl transition duration-300 bg-white '>
-              <Link href='/'>
-                <img
-                  src='/images/hot-sauce-app.png'
-                  alt=''
-                  className=' w-full'
-                />
-              </Link>
-              <div className='p-6 border-t-2 border-blue-500  dark:bg-black overflow-hidden'>
-                <h1 className='md:text-1xl text-xl hover:text-indigo-600 transition duration-200  font-bold text-gray-900 dark:text-gray-400'>
-                  Hot Sauce Collection
-                </h1>
-                <p className='text-gray-700 my-2 hover-text-900 '>
-                  A full stack web app to keep track of my Hot Sauce Collection!
-                </p>
-                <span className='text-xs font-semibold inline-block py-1 px-2 rounded text-white bg-blue-500 dark:bg-gray-700 uppercase last:mr-0 mr-1'>
-                  ExpressJS
-                </span>
-                <span className='text-xs font-semibold inline-block py-1 px-2 rounded text-white bg-blue-500 dark:bg-gray-700 uppercase last:mr-0 mr-1'>
-                  EJS
-                </span>
-                {/* Github and project links */}
-                <div className='mt-3 flex justify-start'>
-                  <Link href='https://powerful-waters-28442.herokuapp.com/sauces'>
-                    <a className='text-blue-500 underline mr-5' target='_blank'>
-                      Live
-                    </a>
-                  </Link>
-                  <Link href='https://github.com/AlexMNet/hotSauceApp'>
-                    <a className='text-blue-500 underline' target='_blank'>
-                      Github
-                    </a>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </ScrollAnimation>
+              </ScrollAnimation>
+            )
+          )}
         </div>
       </div>
     </section>
---
+++ utils/data/technologies.js
@@ -0,0 +1,96 @@
+import {
+  SiJavascript,
+  SiReact,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiMongodb,
+  SiTailwindcss,
+  SiBootstrap,
+  SiCss3,
+  SiGithub,
+  SiHtml5,
+  SiMarkdown,
+  SiStrapi,
+  SiTypescript,
+} from 'react-icons/si';
+
+export const technologies = [
+  {
+    id: 1,
+    link: 'https://www.javascript.com/',
+    title: 'Javascript',
+    icon: <SiJavascript className='hover:scale-110 transform duration-200 ' />,
+  },
+  {
+    id: 2,
+    link: 'https://reactjs.org/',
+    title: 'ReactJS',
+    icon: <SiReact className='hover:scale-110 transform duration-200' />,
+  },
+  {
+    id: 3,
+    link: 'https://reactnative.dev/',
+    title: 'React Native',
+    icon: <SiReact className='hover:scale-110 transform duration-200' />,
+  },
+  {
+    id: 4,
+    link: 'https://nextjs.org/',
+    title: 'NextJS',
+    icon: <SiNextdotjs className='hover:scale-110 transform duration-200' />,
+  },
+  {
+    id: 5,
+    link: 'https://expressjs.com/',
+    title: 'ExpressJS',
+    icon: <SiNodedotjs className='hover:scale-110 transform duration-200' />,
+  },
+  {
+    id: 5,
+    link: 'https://www.mongodb.com/',
+    title: 'MongoDB',
+    icon: <SiMongodb className='hover:scale-110 transform duration-200' />,
+  },
+  {
+    id: 6,
+    link: 'https://tailwindcss.com/',
+    title: 'TailwindCSS',
+    icon: <SiTailwindcss className='hover:scale-110 transform duration-200' />,
+  },
+  {
+    id: 7,
+    link: 'https://getbootstrap.com/',
+    title: 'BootStrap',
+    icon: <SiBootstrap className='hover:scale-110 transform duration-200' />,
+  },
+  {
+    id: 8,
+    link: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
+    title: 'CSS',
+    icon: <SiCss3 className='hover:scale-110 transform duration-200' />,
+  },
+  {
+    id: 9,
+    link: 'https://developer.mozilla.org/en-US/docs/Glossary/HTML5',
+    title: 'HTML5',
+    icon: <SiHtml5 className='hover:scale-110 transform duration-200' />,
+  },
+  {
+    id: 10,
+    link: 'https://www.markdownguide.org/',
+    title: 'Markdown',
+    icon: <SiMarkdown className='hover:scale-110 transform duration-200' />,
+  },
+  {
+    id: 11,
+    link: 'https://github.com/',
+    title: 'Github',
+    icon: <SiGithub className='hover:scale-110 transform duration-200' />,
+  },
+  {
+    id: 12,
+    link: 'https://strapi.io/',
+    title: 'Strapi',
+    icon: <SiStrapi className='hover:scale-110 transform duration-200' />,
+  },
+];
