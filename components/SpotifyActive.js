e71e9359cf32a482b82a19f838d12a2b11af51b9
Alex Maldonado <alex@Alexs-MBP-2021.home>
2022-02-01 03:27:41 -0800

    Spotify Title Regex
    Create a regex function that will do the following to spotify song titles
    1 Remove all text including and after hyphen
    2 Remove all text including and after open parenthesis
    3 Remove all text within parenthesis including parenthesis
    
    Create functions directory for functions

--- components/SpotifyActive.js
+++ components/SpotifyActive.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { BsSpotify } from 'react-icons/bs'
 import TextLoop from 'react-text-loop'
+import { formatSongTitle } from '../functions/functions'
 
 export default function SpotifyActive({ data }) {
     // let isMobile = window.matchMedia('(max-width: 500px)').matches
@@ -10,7 +11,7 @@ export default function SpotifyActive({ data }) {
             <BsSpotify className="text-spotify-green mr-1 text-base md:text-3xl animate-spinSpotify" />{' '}
             <a href={data.songUrl} className="hover:text-spotify-green">
                 <p className="text-xs md:text-base">
-                    {data.title.replace(/\([^()]*\)/g, '')} by
+                    {formatSongTitle(data.title)} by
                     <TextLoop className="ml-1 justify-center items-center">
                         {data.artists.map((artist, idx) => (
                             <span key={idx} className="">
